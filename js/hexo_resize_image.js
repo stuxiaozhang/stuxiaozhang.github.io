0f5f18f51aa5d0e4f3de3e1e5abb3c0923cd8060
stuxiaozhang <zhangxinyan1999@126.com>
2021-01-12 23:25:46 +0800

    Site updated: 2021-01-12 23:25:42

---
+++ js/hexo_resize_image.js
@@ -0,0 +1,51 @@
+function set_image_size(image, width, height) 
+{
+    image.setAttribute("width", width + "px");
+    image.setAttribute("height", height + "px");
+}
+
+function hexo_resize_image()
+{
+    var imgs = document.getElementsByTagName('img');
+    for (var i = imgs.length - 1; i >= 0; i--) 
+    {
+        var img = imgs[i];
+        var src = img.getAttribute('src').toString();
+        var fields = src.match(/\?(\d*x\d*)/);
+        if (fields && fields.length > 1)
+        {
+            var values = fields[1].split("x");
+            if (values.length == 2)
+            {
+                var width = values[0];
+                var height = values[1];
+
+                if (!(width.length && height.length))
+                {
+                    var n_width = img.naturalWidth;
+                    var n_height = img.naturalHeight;
+                    if (width.length > 0)
+                    {
+                        height = n_height*width/n_width;
+                    }
+                    if (height.length > 0)
+                    {
+                        width = n_width*height/n_height;
+                    }
+                }
+                set_image_size(img, width, height);
+            }
+            continue;
+        }
+
+        fields = src.match(/\?(\d*)/);
+        if (fields && fields.length > 1)
+        {
+            var scale = parseFloat(fields[1].toString());
+            var width = scale/100.0*img.naturalWidth;
+            var height = scale/100.0*img.naturalHeight;
+            set_image_size(img, width, height);
+        }
+    }
+}
+window.onload = hexo_resize_image;
